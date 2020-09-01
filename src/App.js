5c6587611a9c291a76cbbcde0508a78cfae52dc5
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-09-01 21:42:29 +0900

    add events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Line } from 'react-chartjs-2';
 
 function App() {
+  const lineRef = useRef(null);
+
   const data = {
     labels: ['Label1', 'Label2', 'Label3', 'Label4', 'Label5'],
-    datasets: [{ label: 'Value', data: [1, 4, 2, 5, 3] }],
+    datasets: [
+      {
+        label: 'Value',
+        data: [1, 4, 2, 5, 3],
+      },
+    ],
   };
-  return <Line data={data} />;
+
+  const onElementsClick = elems => {
+    console.log('onElementsClick');
+    console.log(elems);
+    console.log(lineRef.current);
+  };
+
+  const getElementsAtEvent = elems => {
+    console.log('getElementsAtEvent');
+    console.log(elems);
+  };
+
+  const getElementAtEvent = elems => {
+    console.log('getElementAtEvent');
+    console.log(elems);
+  };
+
+  const getDatasetAtEvent = elems => {
+    console.log('getDatasetAtEvent');
+    console.log(elems);
+  };
+
+  return (
+    <Line
+      ref={lineRef}
+      data={data}
+      onElementsClick={onElementsClick}
+      getElementsAtEvent={getElementsAtEvent}
+      getElementAtEvent={getElementAtEvent}
+      getDatasetAtEvent={getDatasetAtEvent}
+    />
+  );
 }
 
 export default App;
